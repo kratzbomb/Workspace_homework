d0227f537dff506470e3877166c806467b41e4a8
tatyana-belikova <frau-retter@yandex.ru>
2021-07-24 15:48:36 +0300

    dz3 - 1

--- dz3/src/main/java/Apple.java
+++ dz3/src/main/java/Apple.java
@@ -1,6 +1,6 @@
 public class Apple extends Fruit {
 
     public Apple(float weight) {
-        super(1);
+        super(weight);
     }
 }
--- dz3/src/main/java/Main.java
+++ dz3/src/main/java/Main.java
@@ -1,10 +1,24 @@
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Box<Apple> appleBox = new Box<>();
+        ArrayList<Apple> apples = new ArrayList<>();
+        apples.add(new Apple(1));
+        apples.add(new Apple(2));
+        apples.add(new Apple(3));
+        appleBox.setFruitList(apples);
+
+        Box<Orange> orangeBox = new Box<>();
+        orangeBox.addFruit(new Orange(2.5f));
+        orangeBox.addFruit(new Orange(4.5f));
+
+        System.out.println("Apple Box Weight = " + appleBox.getWeight());
+        System.out.println("Orange Box Weight = " + orangeBox.getWeight());
     }
 
     public static void changeArrElements(Object[] array, int a, int b) {
@@ -13,4 +27,6 @@ public class Main {
         array[a] = array[b];
         array[b] = tmp;
     }
+
+
 }
